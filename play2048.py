4fe7ce74b7d5625ace4c93577d087ec7318113cc
Juan Marcos Mendez <jmendez50@mail.valenciacollege.edu>
2016-03-30 23:59:13 -0400

    derp
    tried to make it work with chrome but no go

--- play2048.py
+++ play2048.py
@@ -1,7 +1,24 @@
+#Juan Mendez 
 #TODO: Import the module that will allow you to use Selenium
 #TODO: Import the module that will allow you to use the up, down, left, and right keys on your keyboard
 
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 def play2048( times ):
+    chrome = webdriver.Firefox() #i had to doweload firefox this didnt want to work with chrome
+    chrome.get('http://gabrielecirulli.github.io/2048/')
+    goes = 0
+    web = chrome.find_element_by_tag_name('html')
+
+    for goes in range (times):
+        web.send_keys(Keys.UP)
+        web.send_keys(Keys.RIGHT)
+        web.send_keys(Keys.DOWN)
+        web.send_keys(Keys.LEFT)
+        goes += 1
+    #Was trying to make it run in chrome and i downloaded the chrom tool and all but it didnt work so idk
+        
     #TODO: write code in this function that:
     # 1. opens a game of 2048 from the URL: https://gabrielecirulli.github.io/2048/
     # 2. uses the parameter 'times' to determine how many times your code will try to play the game
